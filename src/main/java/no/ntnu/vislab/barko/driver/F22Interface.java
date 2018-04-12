a66e404ae4864341333889dca9d36865ee908d85
Kristoffer <Kristoffer_rogne@hotmail.com>
2018-04-12 13:11:32 +0200

    rename

---
+++ src/main/java/no/ntnu/vislab/barko/driver/F22Interface.java
@@ -0,0 +1,29 @@
+package no.ntnu.vislab.barko.driver;
+
+
+/**
+ * Interface for a barko f22 projector.
+ */
+public interface F22Interface {
+    int getBrightness();
+
+    int setBrightness(int value);
+
+    int getContrast();
+
+    int setContrast(int value);
+
+    int getLampRuntime(int lampNum);
+
+    int getLampRemaining(int lampNum);
+
+    int getTotalRuntime();
+
+    int getLampStatus(int lampNum);
+
+    int getTemperature();
+
+    int testImageOn(int testImage);
+
+    int testImageOff();
+}
